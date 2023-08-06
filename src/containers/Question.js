987149605e174e91bcc80bf8d12c732d1afb28b2
chasethesky79 <seshadri.bharath@yahoo.com>
2023-08-06 14:43:35 -0500

    Implemented question component

--- src/containers/Question.js
+++ src/containers/Question.js
@@ -1,5 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
 
 const QuestionWrapper = styled.div`
  display: flex;
@@ -18,7 +19,24 @@ const Question = () => {
         error: ''
     }
     const [state, setState] = useState(initialState);
+    const ROOT_API = 'https://api.stackexchange.com/2.2/';
+    const { id } = useParams();
     const { data, loading, error } = state;
+    useEffect(() => {
+        const getQuestion = async () => {
+            try {
+                const URL = `${ROOT_API}questions/${id}?site=stackoverflow`;
+                const data = await fetch(URL, { mode: 'no-cors'})
+                const dataJSON = await data.json();
+                console.log(`ANSWER ${JSON.stringify(dataJSON)}`)
+                setState({...initialState, data: dataJSON, loading: false })
+            } catch(e) {
+                setState({...initialState, error: e.message })
+            }
+          }
+        getQuestion()
+    }, [])
+
     if (loading || error) {
         return (<Alert>{loading ? 'Loading...' : error}</Alert>)
     }
